e498db27e2e3b92db0137797923b2ad172d9d2de
kolaqsq <kolaqsq1707@gmail.com>
2020-06-14 01:09:14 +0300

    Started redact feature

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-    <main id="main">
-        <Add/>
+    <main>
+        <article id="main">
+            <Add/>
 
-        <article class="kanban">
-            <section class="board board_plan">
-                <h1 class="board__title">План: {{quantity.for_board_1}}</h1>
-                <Board class="tasks_plan" id="board-1">
-                    <Card id="1"></Card>
-                    <Card id="2"></Card>
-                </Board>
-            </section>
+            <article class="kanban">
+                <section class="board board_plan">
+                    <h1 class="board__title">План: {{quantity.for_board_1}}</h1>
+                    <Board class="tasks_plan" id="board-1">
+                        <Card id="1"></Card>
+                        <Card id="2"></Card>
+                    </Board>
+                </section>
 
-            <section class="board board_in-work">
-                <h1 class="board__title">В работе: {{quantity.for_board_2}}</h1>
-                <Board class="tasks_plan" id="board-2"></Board>
-            </section>
+                <section class="board board_in-work">
+                    <h1 class="board__title">В работе: {{quantity.for_board_2}}</h1>
+                    <Board class="tasks_plan" id="board-2"></Board>
+                </section>
 
-            <section class="board board_ready">
-                <h1 class="board__title">Готово: {{quantity.for_board_3}}</h1>
-                <Board class="tasks_plan" id="board-3"></Board>
-            </section>
+                <section class="board board_ready">
+                    <h1 class="board__title">Готово: {{quantity.for_board_3}}</h1>
+                    <Board class="tasks_plan" id="board-3"></Board>
+                </section>
+            </article>
         </article>
+
+        <Redact/>
     </main>
 </template>
 
@@ -28,12 +32,14 @@
     import Board from "@/components/Board";
     import Card from "@/components/Card";
     import Add from "@/components/Add";
+    import Redact from "@/components/Redact"
     import {eventBus} from "@/main";
 
 
     export default {
         name: 'Main',
         components: {
+            Redact,
             Board,
             Card,
             Add
---
+++ src/components/Redact.vue
@@ -0,0 +1,31 @@
+<template>
+    <article class="modal">
+        <form action="" class="redact" method="post">
+            <button class="redact__button material-icons" onclick="PopUpHide()">clear
+            </button>
+            <label class="redact__label" for="task-description">Описание</label>
+            <input class="redact__input" id="task-description" name="task-description" placeholder="Лорем ипсум"
+                   required
+                   type="text">
+            <label class="redact__label" for="task-status">Статус</label>
+            <select class="redact__input" id="task-status" name="task-status">
+                <option class="redact__input" value="plan">План</option>
+                <option class="redact__input" value="in-work">В работе</option>
+                <option class="redact__input" value="ready">Готово</option>
+            </select>
+            <label class="redact__label" for="in-charge">Ответственный</label>
+            <input class="redact__input" id="in-charge" name="in-charge" placeholder="Введите имя" type="text">
+            <label class="redact__label" for="task-start">Дата и время начала</label>
+            <input class="redact__input" id="task-start" name="task-start" type="datetime-local">
+            <label class="redact__label" for="task-finish">Дата и время завершения</label>
+            <input class="redact__input" id="task-finish" name="task-finish" type="datetime-local">
+            <input class="redact__submit" type="submit" value="Сохранить">
+        </form>
+    </article>
+</template>
+
+<script>
+    export default {
+        name: "Redact"
+    }
+</script>
